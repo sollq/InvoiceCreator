8fd35b432466ce533dedcb19cd5d6b8b2caea5ec
sollqdev <fafnirsila@mail.ru>
2025-07-19 12:59:00 +0300

    Add Templates and inf

---
+++ Core/Interfaces/INumberGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using Core.Models;
+
+namespace Core.Interfaces;
+
+public interface INumberGeneratorService
+{
+    string GetNextInvoiceNumber(OrganizationType org);
+    string GetNextContractNumber(OrganizationType org);
+}
